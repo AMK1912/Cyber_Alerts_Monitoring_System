0f5eeebc02e5a2290546657b9a3c6da6fffdc421
Amir Mustafa Khan <amirmkhan1912@gmail.com>
2025-04-22 12:00:53 +0530

    Update Program.cs

--- Cyber_Alerts_Monitoring_System/Program.cs
+++ Cyber_Alerts_Monitoring_System/Program.cs
@@ -1,13 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // You can adjust the timeout
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+
 });
 
 var app = builder.Build();
@@ -25,8 +34,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
 
+app.MapStaticAssets();
 app.MapRazorPages();
+app.MapGet("/", () => Results.Redirect("/Login"));
 
 app.Run();
