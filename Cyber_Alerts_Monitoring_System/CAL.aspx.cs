24a1a723186d42748b6d76be27e2e984765f105c
Amir Mustafa Khan <amirmkhan1912@gmail.com>
2025-04-23 12:20:28 +0530

    Create CAL.aspx.cs

---
+++ Cyber_Alerts_Monitoring_System/CAL.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CyberAlertsWebApp
+{
+    public partial class CyberAlertsLocal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // You can add logic to fetch and display alerts here
+        }
+    }
+}
